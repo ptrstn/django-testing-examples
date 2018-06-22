0cd1df06c99015418f78c8d8b1eb957e0ec39dda
Peter Stein <peter.stein@cern.ch>
2018-06-22 18:22:28 +0200

    Replace django-unittest with pytest

---
+++ myapp/tests/selenium_tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from django.contrib.auth.hashers import make_password
+from selenium import webdriver
+
+TEST_USERNAME = "example"
+TEST_PASSWORD = "VerySecurePasswort"
+
+
+@pytest.fixture
+def firefox():
+    """returns a Firefox browser webdriver instance"""
+    options = webdriver.FirefoxOptions()
+    options.add_argument('headless')
+    firefox_webdriver = webdriver.Firefox(firefox_options=options)
+    yield firefox_webdriver
+    firefox_webdriver.quit()
+
+
+@pytest.fixture
+def user(django_user_model):
+    """returns an User object"""
+    return django_user_model.objects.create(
+        username=TEST_USERNAME,
+        password=make_password(TEST_PASSWORD)
+    )
--- myapp/tests/selenium_tests/test_selenium.py
+++ myapp/tests/selenium_tests/test_selenium.py
@@ -1,23 +1,6 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver import FirefoxOptions
 
 
-class MySeleniumTests(StaticLiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        opts = FirefoxOptions()
-        opts.add_argument("--headless")  # Necessary for servers without a display (travis-ci)
-        cls.selenium = WebDriver(firefox_options=opts)
-        cls.selenium.implicitly_wait(10)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()  # close Firefox when tests are done
-        super().tearDownClass()
-
-    def test_login(self):
-        self.selenium.get('%s%s' % (self.live_server_url, reverse('myapp:myview')))
-        assert self.selenium.title == "", "Should be an empty page, because no template"
+def test_anonymous(live_server, firefox):
+    firefox.get('%s%s' % (live_server.url, reverse('myapp:myview')))
+    assert firefox.title == "", "Should be an empty page, because no template"
